cb08d45becdbb29a4eff37d8ba345cf5bce8ba21
Mansi Rawat <mansir.codxegy@gmail.com>
2025-07-28 00:08:08 +0530

    upload

--- src/components/DashboardWithApi.tsx
+++ src/components/DashboardWithApi.tsx
@@ -2,7 +2,6 @@ import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 import { Button } from './ui/button';
 import { Badge } from './ui/badge';
 import { 
-  Eye, 
   Edit, 
   Copy, 
   Trash2, 
